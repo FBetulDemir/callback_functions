ad849d42efc85bb86c25b04fb4a196c506e0adfc
FBetulDemir <fatmabetuld@gmail.com>
2024-10-02 07:44:18 +0200

    Added callback functions

--- aboutMe.js
+++ aboutMe.js
@@ -1,6 +1,44 @@
 const displayFAvoriteTakeoutFood = (callback) => {
+  console.log("Fetching data ...");
   setTimeout(() => {
-    console.log("My favorite takeout food is sushi.");
-    callback();
-  }, 1000);
+    const data = "My favorite takeout food is sushi.";
+    console.log("After 2 seconds");
+    callback(data);
+  }, 2000);
 };
+
+const displayFAvoriteMovie = (callback) => {
+  console.log("Fetching data ...");
+  setTimeout(() => {
+    const data = "My favorite movie is Into The Wild.";
+    console.log("After 2.5 seconds");
+    callback(data);
+  }, 2500);
+};
+
+const displayDreamDestination = (callback) => {
+  console.log("Fetching data ...");
+  setTimeout(() => {
+    const data = "I dream of visiting Japan.";
+    console.log("After 3 seconds");
+    callback(data);
+  }, 3000);
+};
+
+const displayData = (data) => {
+  console.log(data);
+};
+
+// displayFAvoriteTakeoutFood(displayData);
+// displayFAvoriteMovie(displayData);
+// displayDreamDestination(displayData);
+
+displayFAvoriteTakeoutFood((data) => {
+  displayData(data);
+  displayFAvoriteMovie((data) => {
+    displayData(data);
+    displayDreamDestination((data) => {
+      displayData(data);
+    });
+  });
+});
